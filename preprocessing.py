aebacc22abadd585cf4d9c96343370bca4be0e41
arvene <arvene241@gmail.com>
2023-02-08 18:22:24 +0800

    final commit

--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,10 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet as wn
 from collections import defaultdict
 import regex as re
+from tensorflow import keras
 import keras.utils as keras_image
+import string
+
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
@@ -44,3 +47,36 @@ def preprocess_txt(text):
   text = " ".join(Final_words)
 
   return text
+
+# def preprocess_txt(text):
+#   word_Lemmatized = WordNetLemmatizer()
+#   text = str(text)
+#
+#   no_punct = "".join([c for c in text if c not in string.punctuation])
+#   url_re_1 = r"\b(?:https?://|www\.)[a-z0-9-]+(\.[a-z0-9-]+)+(?:[/?].*)?"  # remove a maioria das urls
+#   url_re_2 = r"(w{3}\.)*[a-zA-Z0-9]+\.{1}(co){1}[m]{0,1}\s{0,1}"  # remove any.com urls
+#   url_re_3 = r"(w{3}\.)*[a-zA-Z0-9]+\.{1}(net){1}\s{0,1}"  # remove any.net urls
+#   digits = r'[0-9]'  # remove digitos
+#   excess_whitespace_removed = ' '.join(text.split())
+#
+#   s1 = re.sub(url_re_1, "", excess_whitespace_removed)
+#   s2 = re.sub(url_re_2, "", s1)
+#   s3 = re.sub(url_re_3, "", s2)
+#   s4 = re.sub(digits, "", s3)
+#   text = s4.translate(str.maketrans('', '', string.punctuation))
+#
+#   text = text.lower()
+#   stop = [w for w in text if w not in stopwords.words('english')]
+#   pat = r'\b(?:{})\b'.format('|'.join(stop))
+#   text = text.replace(pat, '')
+#   text = text.replace(r'\s+', ' ')
+#
+#   text = word_tokenize(text)
+#
+#   Final_words = []
+#   final_word = " ".join([word_Lemmatized.lemmatize(word) for word in text])
+#   Final_words.append(final_word)
+#
+#   text = " ".join(Final_words)
+#
+#   return text
